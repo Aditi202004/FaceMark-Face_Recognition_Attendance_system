c5d43101926943eaef1ef9a4a6bd0baefeaeabfb
Mishajain1110 <117731761+Mishajain1110@users.noreply.github.com>
2023-11-08 23:55:30 +0530

    Made changes

---
+++ facerecognition/static/js/alert.js
@@ -0,0 +1,9 @@
+const myButton = document.getElementById("myButton");
+myButton.onclick = function () {
+  alert("New student successfully added!");
+};
+
+const attendanceButton = document.getElementById("attendance"); 
+attendanceButton.onclick = function () {
+  alert("Your attendance is marked!");
+};
